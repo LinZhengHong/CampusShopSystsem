ff6b3012e52274f978b88cf92165802c1d74bd09
LinZhenHong <2698961980@qq.com>
2020-07-01 22:48:14 +0800

    店铺列表和店铺管理的前端开发

--- src/main/java/com/linzhenghong/o2o/web/shopadmin/ShopManagementController.java
+++ src/main/java/com/linzhenghong/o2o/web/shopadmin/ShopManagementController.java
@@ -55,7 +55,7 @@ public class ShopManagementController {
             Object currentShopObj=request.getSession().getAttribute("currentShop");
             if (currentShopObj==null){
                 modelMap.put("redirect",true);
-                modelMap.put("url","/shop/shoplist");
+                modelMap.put("url","/o2o/shopadmin/shoplist");
             }else {
                 Shop currentShop=(Shop) currentShopObj;
                 modelMap.put("redirect",false);
