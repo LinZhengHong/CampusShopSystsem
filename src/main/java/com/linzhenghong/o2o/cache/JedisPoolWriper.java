0a7bdb79b920bed568e761d2ff30878b93075d3f
LinZhenHong <2698961980@qq.com>
2020-09-10 01:25:03 +0800

    Redis缓存配置

---
+++ src/main/java/com/linzhenghong/o2o/cache/JedisPoolWriper.java
@@ -0,0 +1,41 @@
+package com.linzhenghong.o2o.cache;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * Redis连接池
+ * @author LinZhenHong
+ */
+public class JedisPoolWriper {
+
+    /**
+     * Redis连接池对象
+     */
+    private JedisPool jedisPool;
+
+
+    public JedisPoolWriper(final JedisPoolConfig jedisPoolConfig,final String host, final int port) {
+        try{
+            jedisPool=new JedisPool(jedisPoolConfig,host,port);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取Redis连接池对象
+     * @return
+     */
+    public JedisPool getJedisPool(){
+        return jedisPool;
+    }
+
+    /**
+     * 注入Redis连接池对象
+     * @param jedisPool
+     */
+    public void setJedisPool(JedisPool jedisPool) {
+        this.jedisPool = jedisPool;
+    }
+}
